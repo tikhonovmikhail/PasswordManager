42a78dbc746114bf15646b9e89b2e086cc6e08f7
misha123 <misha123@misha123-System-Product-Name.(none)>
2016-05-14 06:23:59 +0800

    Running unit tests added

--- src/PasswordManager.cpp
+++ src/PasswordManager.cpp
@@ -1,4 +1,12 @@
+#include "easyunit/testharness.h"
+#include "UnitTestProperties.h"
+
 int main()
 {
+#ifdef RUN_TESTS
+	TestRegistry::runAndPrint();
 	return 0;
+#else
+	return 0;
+#endif // RUN_TESTS
 }
